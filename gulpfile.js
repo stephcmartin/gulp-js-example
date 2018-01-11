253ccb99d8d533b710d0c808b7197ef6ad59a5a8
stephcmartin <martin.stephaniechow@gmail.com>
2018-01-11 11:45:58 -0500

    Added `copyHTML` function to produce `dist` folder
    - When you run `copyHTML` function it copies all HTML files within
    src and created a `dist` folder for deployment

--- gulpfile.js
+++ gulpfile.js
@@ -8,3 +8,20 @@ const gulp = require ('gulp')
  gulp.dest - point to the folder to output
  gulp.watch - watch files and folders for changes
 */
+
+// logs message
+
+gulp.task('message', function(){
+    return console.log('Gulp is running...')
+});
+
+gulp.task('default', function(){
+    return console.log('Gulp is running...')
+});
+
+// Copy all HTML files and creates it own dist folder for publishing
+
+gulp.task('copyHTML', function(){
+    gulp.src('src/*.html')
+    .pipe(gulp.dest('dist'))
+})
