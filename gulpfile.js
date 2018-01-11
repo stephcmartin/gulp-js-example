563946313bf040bd41e11400b60a7fb7cc88a26c
stephcmartin <martin.stephaniechow@gmail.com>
2018-01-11 11:39:02 -0500

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,10 @@
+const gulp = require ('gulp')
+
+/*
+ GULP TOP LEVEL FUNCTIONS:
+
+ gulp.task - allows use to define tasks
+ gulp.src - point to the files to use
+ gulp.dest - point to the folder to output
+ gulp.watch - watch files and folders for changes
+*/
